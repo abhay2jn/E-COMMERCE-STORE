11c6a44a87a6fddcb2811e901b0dc7e24cd71593
abhay <abhayforwork02@gmail.com>
2024-08-13 00:01:39 +0530

    register and login user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,5 +4,5 @@ import { createUser, loginUser } from "../controllers/userController.js";
 const router = express.Router();
 
 router.route("/").post(createUser);
-router.post("/auth", loginUser)
+router.post("/auth", loginUser);
 export default router;
