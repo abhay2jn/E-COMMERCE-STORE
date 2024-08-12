822cff19b5338239800a9944733a2bd2b7677be8
abhay <abhayforwork02@gmail.com>
2024-08-12 23:38:10 +0530

    login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -29,4 +29,26 @@ const createUser = asyncHandler(async (req, res) => {
         throw new Error("Invalid user data");
     }
 })
-export { createUser };
+const loginUser = asyncHandler(async(req, res) => {
+    const [email, password] = req.body
+
+    const existingUser = await User.findOne({email});
+    if (existingUser) {
+        const isPasswordValid = await bcrypt.compare(password, existingUser.password)
+        if (isPasswordValid) {
+            createToken(res, existingUser._id);
+
+            res.status(202).json({
+                _id: existingUser._id,
+                username: existingUser.username,
+                email: existingUser.email,
+                isAdmin: existingUser.isAdmin,
+            });
+            return;
+        }
+
+    }
+})
+
+
+export { createUser, loginUser };
