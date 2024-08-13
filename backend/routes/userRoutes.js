5ead4ac9bca18367f0e4c53020a648f6f5340e38
abhay <abhayforwork02@gmail.com>
2024-08-14 01:08:30 +0530

    routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createUser, loginUser, logoutUser } from "../controllers/userController.js";
+import { createUser, loginUser, logoutUser, getAllUsers } from "../controllers/userController.js";
+import { authenticate, authorizedAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").post(createUser);
+router.route("/").post(createUser).get(authenticate, authorizedAdmin, getAllUsers);
 router.post("/auth", loginUser);
 router.post("/logout", logoutUser);
 export default router;
