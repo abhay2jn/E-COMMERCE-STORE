9b8661c5490f4548d24fcd27ac0e828e9085c683
abhay <abhayforwork02@gmail.com>
2024-08-17 01:15:06 +0530

    web page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
-import "./index.css";
+import { Outlet } from "react-router-dom";
+import Navigation from "./pages/Auth/Navigation";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/ReactToastify.css"
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello there!
-    </h1>
+    <>
+      <ToastContainer />
+      <Navigation />
+      <main className="py-3">
+        <Outlet />
+      </main>
+    </>
   )
 }
 
