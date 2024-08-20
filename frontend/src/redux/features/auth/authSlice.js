19dccfefe6c6dead68f378cc6c6a0372f71b6eca
abhay <abhayforwork02@gmail.com>
2024-08-20 23:25:25 +0530

    creating slice

--- frontend/src/redux/features/auth/authSlice.js
+++ frontend/src/redux/features/auth/authSlice.js
@@ -5,25 +5,22 @@ const initialState = {
     userInfo: localStorage.getItem("userInfo") ? JSON.parse(localStorage.getItem("userInfo")) : null,
 }
 
-const reducers = {
-    setCredientials: (state, action) => {
-        state.userInfo = action.payload;
-        localStorage.setItem("userInfo", JSON.stringify(action.payload));
-        const expirationTime = new Date().getTime() + 30 * 24 * 60 * 60 * 1000;
-        localStorage.setItem("expirationTime", expirationTime)
-    }
-}
-
-
 
 const authSlice = createSlice({
     name: "auth",
     initialState,
-    reducers,
-    logout: (state) => {
+    reducers: {
+        setCredientials: (state, action) => {
+            state.userInfo = action.payload;
+            localStorage.setItem("userInfo", JSON.stringify(action.payload));
+            const expirationTime = new Date().getTime() + 30 * 24 * 60 * 60 * 1000;
+            localStorage.setItem("expirationTime", expirationTime)
+        },
+        logout: (state) => {
         state.userInfo = null;
         localStorage.clear();
-    },
+        }
+    }
 })
 
 export const { setCredientials, logout} = authSlice.actions;
