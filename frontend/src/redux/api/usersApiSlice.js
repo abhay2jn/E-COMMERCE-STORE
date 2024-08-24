0c84e49bea9e3960a5f3321f316b9a30e103d96e
abhay <abhayforwork02@gmail.com>
2024-08-25 00:30:58 +0530

    api slice

--- frontend/src/redux/api/usersApiSlice.js
+++ frontend/src/redux/api/usersApiSlice.js
@@ -1,15 +1,29 @@
 import { apiSlice } from "./apiSlice";
 import { USERS_URL } from "../constatnts";
+import { data } from "autoprefixer";
 
 export const usersApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        login: builder.mutation({
-            query: (data) => ({
-                url: `${USERS_URL}/auth`,
-                method: "POST",
-                body: data,
-            })
-        })
-    })
-})
-export const { useLoginMutation } = usersApiSlice
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/auth`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}/logout`,
+        method: "POST",
+      }),
+    }),
+    register: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+export const { useLoginMutation, useLogoutMutation, useRegisterMutation } = usersApiSlice;
