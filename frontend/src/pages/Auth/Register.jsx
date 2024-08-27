d059c2a4ae3dba7546a22939527da52c00549f49
abhay <abhayforwork02@gmail.com>
2024-08-27 14:58:32 +0530

    Register

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -46,7 +46,7 @@ const Register = () => {
   return (
     <section className="pl-[10rem] flex flex-wrap">
       <div className=" mr-[4rem] mt-[5rem]">
-        <h1 className=" text-2xl font-semibold mb-4">Register</h1>
+        <h1 className=" text-2xl font-semibold mb-4 text-white">Register</h1>
         <form onSubmit={submitHandler} className=" container w-[40rem]">
           <div className=" my-[2rem]">
             <label
@@ -135,6 +135,10 @@ const Register = () => {
           </p>
         </div>
       </div>
+      <img
+      src="https://images.unsplash.com/photo-1576502200916-3808e07386a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2065&q=80"
+      alt=""
+      className="h-[65rem] w-[59%] xl:block md:hidden sm:hidden rounded-lg" />
     </section>
   );
 };
