9e41440a87028b04706627be8295fb499b5ea43f
abhay <abhayforwork02@gmail.com>
2024-08-29 15:21:39 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -10,11 +11,11 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use("/api/users",userRoutes);
-
+app.use("/api/users", userRoutes);
+app.use("/api/category", categoryRoutes);
 
 app.listen(port, () => {
-    console.log(`server up on port: ${port}`);
-})
+  console.log(`server up on port: ${port}`);
+});
