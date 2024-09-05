9756978daae585c766588600e7ab1eaa9d5dd1eb
abhay <abhayforwork02@gmail.com>
2024-09-05 23:07:03 +0530

    store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,17 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query/react";
 import { apiSlice } from "./api/apiSlice";
-import authReducer from "./features/auth/authSlice"
+import authReducer from "./features/auth/authSlice";
+import favoritesReducer from "../redux/features/favorites/favoriteSlice";
+import { getFavoritesFromLocalStorage } from "../Utils/localStorage";
+
+const initialFavorites = getFavoritesFromLocalStorage() || [];
 
 const store = configureStore({
-    reducer: {
-        [apiSlice.reducerPath]: apiSlice.reducer,
-        auth: authReducer,
-    },
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
+    favorites: favoritesReducer,
+  },
 
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
-    devTools: true,
-})
+  preloadedState: {
+    favorites: initialFavorites,
+  },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
+});
 
 setupListeners(store.dispatch);
 export default store;
